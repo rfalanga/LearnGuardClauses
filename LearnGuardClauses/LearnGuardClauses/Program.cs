c3afb0f976870502a2385ede44a588817d3507ad
Rod Falanga <rfalanga@users.noreply.github.com>
2023-11-23 07:37:21 -0700

    trying just the barebones method
    Note: this crashes, because no error trapping is used

--- LearnGuardClauses/LearnGuardClauses/Program.cs
+++ LearnGuardClauses/LearnGuardClauses/Program.cs
@@ -1,4 +1,9 @@
 ﻿using LearnGuardClauses;
 
+// First, an order that works fine - the Happy Path!
 var o = new Order("Some Order", 1, 10, (decimal)0.25, DateTime.Now);
 Console.WriteLine(o.ToString());
+
+// Now, let's start messing things up
+var badName = new Order(string.Empty, 1, 10, (decimal)0.25, DateTime.Now);
+Console.WriteLine(badName.ToString());
